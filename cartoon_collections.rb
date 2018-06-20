5a211520a248c022e83777e9093bca13b3580510
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-20 19:03:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index+1} #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(ele)# code an argument here
   # Your code here
+  ele.map do |e|
+    puts e.capitalize + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any { |call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
